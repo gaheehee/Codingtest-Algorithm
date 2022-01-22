fe0cb8f26081d367226c2a0cef7af6de20f8a1eb
Jeong Ga Hee <jghgahee12@gmail.com>
2022-01-22 19:55:28 +0900

    [N11650}: 좌표 정렬하기

---
+++ baekjoon-Algorithm/algorithm-middle/sorting/N11650.py
@@ -0,0 +1,19 @@
+'''
+2차원 평면 위의 점 N개가 주어진다. 
+
+좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+'''
+
+import sys
+
+n = int(sys.stdin.readline())
+arr = []
+
+for _ in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    arr.append((a,b))
+
+arr.sort()
+
+for a, b in arr:
+    print(a, b)
