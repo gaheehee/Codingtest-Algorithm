96d8fc13abc9dec0ae7ec176407102511c5a87f7
Jeong Ga Hee <jghgahee12@gmail.com>
2022-01-24 16:34:45 +0900

    [N15651]: backtracking - N과 M

---
+++ baekjoon-Algorithm/algorithm-middle/backtracking/N15651.py
@@ -0,0 +1,22 @@
+'''
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+1부터 N까지 자연수 중에서 M개를 고른 수열
+같은 수를 여러 번 골라도 된다.
+'''
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+result = []
+
+def backtracking(k):
+    if k == m:
+        print(' '.join(map(str, result)))
+        return
+    
+    for i in range(1, n+1):
+        result.append(i)
+        backtracking(k + 1)
+        result.pop()
+
+backtracking(0)
