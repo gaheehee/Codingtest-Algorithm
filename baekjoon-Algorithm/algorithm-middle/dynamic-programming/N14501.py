1cac46074f86cde4f7cbb657577fe46adddb76ef
Jeong Ga Hee <jghgahee12@gmail.com>
2023-06-09 10:01:47 +0900

    [N14501]: DP top-down

--- baekjoon-Algorithm/algorithm-middle/dynamic-programming/N14501.py
+++ baekjoon-Algorithm/algorithm-middle/dynamic-programming/N14501.py
@@ -1,4 +1,4 @@
-import sys
+'''import sys
 
 n = int(sys.stdin.readline())
 schedule = []
@@ -12,3 +12,23 @@ for i in range(n):
         dp[j] = max(dp[j], dp[i] + schedule[i][1])
 
 print(dp[-1])
+'''
+
+# dp문제 중 top-down은 재귀이용한다
+
+import sys
+
+n = int(sys.stdin.readline())
+schedule = []
+dp = [0 for _ in range(n+1)]
+
+for _ in range(n):
+    schedule.append(list(map(int, sys.stdin.readline().split())))
+
+for i in range(n-1, -1, -1):
+    if i + schedule[i][0] > n:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(dp[i + 1], schedule[i][1] + dp[i + schedule[i][0]])
+
+print(dp[0])
