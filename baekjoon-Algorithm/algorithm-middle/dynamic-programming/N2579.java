0a1942811b989c1c27172e264d2b8ebec406d7ce
Jeong Ga Hee <jghgahee12@gmail.com>
2023-07-04 21:25:08 +0900

    [N2579]: dp - java

---
+++ baekjoon-Algorithm/algorithm-middle/dynamic-programming/N2579.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.io.*;
+
+public class N2579 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] stair = new int[N + 1];
+        int[] dp = new int[N + 1];
+
+        for (int i = 1; i < N + 1; i++) stair[i] = Integer.parseInt(br.readLine());
+
+        if (N <= 2) {
+            dp[N] = Arrays.stream(stair).sum();
+
+        } else {
+
+            dp[1] = stair[1];
+            dp[2] = stair[1] + stair[2];
+
+            for (int i = 3; i < N + 1; i++) {
+                dp[i] = Math.max(dp[i-3] + stair[i-1] + stair[i], dp[i-2] + stair[i]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
