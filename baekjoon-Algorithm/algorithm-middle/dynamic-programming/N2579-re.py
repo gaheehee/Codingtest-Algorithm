e14ac580508ef538261176b2b1e1455e3927f100
Jeong Ga Hee <jghgahee12@gmail.com>
2023-07-04 16:55:48 +0900

    [N2579]: dp - py

---
+++ baekjoon-Algorithm/algorithm-middle/dynamic-programming/N2579-re.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+stair = [ int(input()) for _ in range(N)]
+
+if N <= 2:
+    print(sum(stair))
+
+else:
+    dp = [0] * N
+    dp[0] = stair[0]
+    dp[1] = stair[0] + stair[1]
+
+    for i in range(2, N):
+        dp[i] = (max(dp[i-3] + stair[i-1] + stair[i], dp[i-2] + stair[i]))
+
+    print(dp[N-1])
