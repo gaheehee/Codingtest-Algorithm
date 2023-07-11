f325a43b1dccb5104f3aa303e04787b81ffc6020
Jeong Ga Hee <jghgahee12@gmail.com>
2023-07-11 15:42:49 +0900

    [N9663]: backtracking - py

--- baekjoon-Algorithm/algorithm-middle/brute-force/N9663.py
+++ baekjoon-Algorithm/algorithm-middle/brute-force/N9663.py
@@ -1,40 +1,35 @@
-# N-Queen
-'''
-N-Queen 문제는 크기가 N × N인 체스판 위에 퀸 N개를 서로 공격할 수 없게 놓는 문제이다.
-
-N이 주어졌을 때, 퀸을 놓는 방법의 수를 구하는 프로그램을 작성하시오.
-'''
-
-# depth 0부터 시작해서 재귀적으로 진행. (depth가 n이랑 같아지면 count)
-# 배열의 주소값이 행, 요소값이 열
-# 배열에 값을 넣고 공격할 수 없는 위치(True)라면 재귀(depth+1) 호출 
-
 import sys
 
-n = int(sys.stdin.readline())
-queen = [0]*n
-count = 0
+input = sys.stdin.readline
 
-def check(depth):
-    for i in range(depth):
-        if queen[depth] == queen[i] or abs(queen[depth] - queen[i]) == depth - i:
+N = int(input())
+
+cnt = 0
+row = [0] * N
+
+
+def is_promising(x):
+    for i in range(x):
+        # 세로 체크, 대각선 체크
+        if row[x] == row[i] or abs(row[x] - row[i]) == x - i:
             return False
+
     return True
 
-def dfs(depth):
-    
-    # 탐색 break 
-    if depth == n:
-        global count
-        count += 1
+
+def n_queens(x):
+    global cnt
+
+    if x == N:
+        cnt += 1
         return
 
-    for i in range(n):
-        queen[depth] = i
-        if check(depth):
-            dfs(depth + 1)
+    for i in range(N):
+        row[x] = i  # (x, i)에 퀸을 놓음
+        # 유망성 판단: 다음 칸에 놓을 수 있는 경우가 있는지
+        if is_promising(x):
+            n_queens(x + 1)
 
-dfs(0)
-print(count)
 
-# for i in range(0)은 포문이 돌지 않는다.
+n_queens(0)
+print(cnt)
